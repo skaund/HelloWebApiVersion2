fe05979e6d9ed77bc6a1e5055aa04307d6ccc959
Danil Vasilenko <danyav2@yandex.ru>
2021-06-03 20:25:06 +0400

    Добавьте файлы проекта.

---
+++ HelloWebApi/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public string Company { get; set; }
+
+    public DateTime Creature { get; set; }
+}
---
+++ HelloWebApi/Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using HelloWebApi.Models;
+
+namespace WebAPIApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string con = "Server=(localdb)\\mssqllocaldb;Database=usersdbstore;Trusted_Connection=True;";
+            // устанавливаем контекст данных
+            services.AddDbContext<UsersContext>(options => options.UseSqlServer(con));
+            services.AddControllers(); // используем контроллеры без представлений
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
